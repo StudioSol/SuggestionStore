89df2714f554bc4d60281c6a203fcf21eed6ad90
Ivan Júnior <ivan.junior@studiosol.com.br>
2013-06-03 11:17:20 -0300

    Build updated

--- dist/SuggestionStore.js
+++ dist/SuggestionStore.js
@@ -33,6 +33,24 @@
     }
     exports.getDocumentSet = getDocumentSet;
 
+    function groupBy(items, keyFn) {
+        var result = {}, i = 0, v, k;
+
+        for (; i < items.length; i += 1) {
+            v = items[i];
+            k = keyFn(v);
+
+            if (k in result) {
+                result[k].push(v);
+            } else {
+                result[k] = [v];
+            }
+        }
+
+        return result;
+    }
+    exports.groupBy = groupBy;
+
 }(window.SuggestionStore = window.SuggestionStore || {}));
 ;(function(exports) {
      var indexedDB = window.indexedDB || window.mozIndexedDB ||
